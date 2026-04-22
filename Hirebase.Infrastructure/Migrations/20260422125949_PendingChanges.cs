d2a4d7898739c9fead891e0e25f1249f95a3f21a
SaanaValkama <saanavalkama@outlook.com>
2026-04-22 16:08:56 +0300

    fix:correct check for token

---
+++ Hirebase.Infrastructure/Migrations/20260422125949_PendingChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hirebase.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_TokenHash",
+                table: "RefreshTokens",
+                column: "TokenHash",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_TokenHash",
+                table: "RefreshTokens");
+        }
+    }
+}
